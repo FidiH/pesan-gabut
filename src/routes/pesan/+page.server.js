950f4244f2a1545748f09b26315837be2427f3d8
fidi <fidihartato.pidi@gmail.com>
2026-04-16 19:44:14 +0700

    fix: caching fetch data

--- src/routes/pesan/+page.server.js
+++ src/routes/pesan/+page.server.js
@@ -1,11 +1,11 @@
 import {getDataPesan} from "$lib/simpanKeSupabase.js"
 
 export async function load({ setHeaders }) {
-  const pesan = await getDataPesan();
+  const data = await getDataPesan();
 
   setHeaders({
     "cache-control": "public, max-age=60" // cache 60 detik
   });
 
-  return { pesan: pesan ?? [] };
+  return { data: data ?? [] };
 }
