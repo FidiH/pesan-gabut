9a75bcd3da3a84437b8ab77496ef1f5f4f4d32e9
fidi <fidihartato.pidi@gmail.com>
2026-04-17 13:25:36 +0700

    style: rewrite style form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import { enhance } from "$app/forms";
   import { onMount } from "svelte";
-
+  
+  import BG from "$lib/assets/Bg.mp4";
+  
   const COOLDOWN_MS = 2 * 60 * 1000;
   const STORAGE_KEY = "last_submit";
 
@@ -51,6 +53,11 @@
     }
     previewUrl = URL.createObjectURL(file);
   }
+  
+  function hapusFile(){
+    fileInput = null;
+    previewUrl = null;
+  }
 
   function resetForm() {
     formRef?.reset();
@@ -63,11 +70,11 @@
 
     return async ({ result, update }) => {
       loading = false;
-
+      console.log(result)
       if (result.type === "failure" || result.type === "error") {
         pesan = {
           tipe: "error",
-          teks: result.data?.error ?? "Terjadi kesalahan, coba lagi.",
+          teks: result.data?.pesan ?? "Terjadi kesalahan, coba lagi.",
         };
         localStorage.removeItem(STORAGE_KEY);
         sisaWaktu = 0;
@@ -87,12 +94,21 @@
   };
 </script>
 
-<section>
+<section class="h-screen w-screen">
+  
+  <div id="bgVideo" class="fixed -z-50 flex justify-center items-center">
+    <video class="scale-125 fixed top-0 left-0 w-screen h-screen" src="{BG}" muted autoplay loop></video>
+    <div class="absolute top-0 left-0 bg-gradient-to-b from-gray-500/50 to-gray-700/10 z-50 w-screen h-screen"></div>
+  </div>
+  
   {#if pesan}
-    <p>{pesan.teks}</p>
+  <div class="p-4">
+    <p class="p-2 text-sm rounded-xl bg-gradient-to-tr from-orange-400/80 to-amber-200/70 border border-amber-300 text-white">{pesan?.teks}</p>
+  </div>
   {/if}
 
   <form
+    class="p-7"
     bind:this={formRef}
     action="/"
     method="POST"
@@ -100,18 +116,31 @@
     use:enhance={submitEnhance}
   >
     <div>
-      <label for="nama">Nama</label>
-      <input id="nama" required type="text" name="nama" disabled={loading || sisaWaktu > 0} />
+      <!--<label for="nama">Nama</label>-->
+      <input 
+        class="inputStyle" 
+        id="nama" 
+        placeholder="ex: Pidoy_kece"
+        required 
+        type="text" 
+        name="nama" 
+        disabled={loading || sisaWaktu > 0} />
     </div>
 
     <div>
-      <label for="pesan">Pesan</label>
-      <textarea id="pesan" name="pesan" disabled={loading || sisaWaktu > 0}></textarea>
+      <!--<label for="pesan">Pesan</label>-->
+      <textarea 
+        class="inputStyle h-28"
+        id="pesan" 
+        name="pesan" 
+        placeholder="ex: P, infokan"
+        disabled={loading || sisaWaktu > 0}></textarea>
     </div>
 
     <div>
-      <label for="file">File Gambar</label>
+      <label for="file" class="px-2 py-1 bg-sky-400/90 rounded-lg text-white font-bold my-5 w-fit block">{previewUrl ? "Ganti" : "Gambar"}</label>
       <input
+        hidden="true"
         id="file"
         bind:this={fileInput}
         accept=".jpg, .png, .jpeg, .webp"
@@ -121,12 +150,20 @@
         onchange={handleFileChange}
       />
       {#if previewUrl}
-        <img src={previewUrl} alt="preview" />
+        <div class="relative pb-50">
+          <button 
+            disabled={loading || sisaWaktu > 0}
+            onclick="{hapusFile}"
+            type="button" 
+            class="absolute -top-3 -right-3 rounded-full z-50 bg-gray-100 rotate-45 flex justify-center items-center text-2xl w-7 h-7 text-red-500">+</button>
+         <img class="rounded-xl drop-shadow-lg" src={previewUrl} alt="preview" />
+        </div>
       {/if}
     </div>
 
-    <div>
-      <button type="submit" disabled={loading || sisaWaktu > 0}>
+    <div class="fixed left-0 w-screen bg-gradient-to-b from-gray-50/10 to-gray-400/80 bottom-0 p-7 flex justify-center rounded-t-3xl items-center h-24">
+      <button class="bg-sky-400/80 py-1 px-3 font-bold
+       rounded-xl shadow text-white block" type="submit" disabled={loading || sisaWaktu > 0}>
         {#if loading}
           Mengirim...
         {:else if sisaWaktu > 0}
@@ -139,3 +176,13 @@
     </div>
   </form>
 </section>
+
+
+<style>
+  @reference "tailwindcss";
+  
+  .inputStyle{
+    @apply w-full px-2 py-1 bg-gray-50/40 text-gray-50 font-bold rounded-lg mt-7 outline-0;
+    filter: drop-shadow(5px 5px 0 #0909097a);
+  }
+</style>
