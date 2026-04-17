268ab7c06c0001c2d5700f61ca03b4deb26f40c4
fidi <fidihartato.pidi@gmail.com>
2026-04-17 12:54:14 +0700

    format: buang kode console.log

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -46,7 +46,6 @@ export const actions = {
     
     // --- KIRIM KE SUPABASE---
     const berhasil = await simpanKeSupabase(formatData);
-    console.log(berhasil)
     if(!berhasil?.sukses) {
       return fail(500, {pesan: berhasil?.error?.message+", cek koneksi"})
     }
